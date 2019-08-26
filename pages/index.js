a804540b4b6f539df30854c0f8436c0888e2360f
Rennê Ruan Alves Oliveira <renne.oliveira@synapsebrasil.com.br>
2019-08-26 17:53:46 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import content from '../content/home.md';
+
+export default class Home extends Component{
+    render(){
+        let {html , attributes: {title, cats}} = content;
+        return(
+            <article>
+                <h1>{title}</h1>
+                <div dangerouslySetInnerHTML={{ __html: html }}/>
+                <ul>
+                    { cats.map((cat, k) => (
+                        <li key={k}>
+                            <h2>{cat.name}</h2>
+                            <p>{cat.description}</p>
+                        </li>
+                    ))}
+                </ul>
+            </article>
+        );
+    }
+}
+
